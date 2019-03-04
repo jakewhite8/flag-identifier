438c1bc946d36fc1be5b92a2d0383878155fc491
Jake White <jwhite52@gmail.com>
2019-03-04 13:11:18 -0500

    Add ColorFilter component

--- components/FilterSection.js
+++ components/FilterSection.js
@@ -1,17 +1,32 @@
 import css from '../styles.css'
+import Link from 'next/link'
+
+const ColorFilter = (props) => { 
+// if this color is included in the query/params, this link 
+    // should remove the color from the query/params
+// else this color should be added to the params/query
+    // the link includes the color
+  const paramLink = '/?color=' // + whatever is decided in logic above
+  return (
+    <Link href={paramLink}>
+      <a>{props.color}</a>
+    </Link>
+  )
+}
+
 
 const FilterSection = () => (
   <div>
     <h3>Color:</h3>
     <div className={css.colorFilters}>
-      <a>Red</a>
-      <a>Green</a>
-      <a>Blue</a>
-      <a>Orange</a>
-      <a>Purple</a>
-      <a>Yellow</a>
-      <a>White</a>
-      <a>Black</a>
+      <ColorFilter color='red' />
+      <ColorFilter color='green' />
+      <ColorFilter color='blue' />
+      <ColorFilter color='orange' />
+      <ColorFilter color='purple' />
+      <ColorFilter color='yellow' />
+      <ColorFilter color='white' />
+      <ColorFilter color='black' />
     </div>
   </div>
 )
